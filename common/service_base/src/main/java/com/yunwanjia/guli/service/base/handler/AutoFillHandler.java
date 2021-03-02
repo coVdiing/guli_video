16dd925b0412c522825bd27c881e168ccdd8bf18
lijw <113714806@qq.com>
2021-03-02 11:15:09 +0800

    实现MetaObjectHandler拦截器，新增或修改数据时自动填充gmt_create,gmt_modified字段

---
+++ common/service_base/src/main/java/com/yunwanjia/guli/service/base/handler/AutoFillHandler.java
@@ -0,0 +1,22 @@
+package com.yunwanjia.guli.service.base.handler;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import org.apache.ibatis.reflection.MetaObject;
+
+import java.util.Date;
+
+/**
+ * @author lijw
+ */
+public class AutoFillHandler implements MetaObjectHandler {
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        this.setFieldValByName("gmtCreate", new Date(), metaObject);
+        this.setFieldValByName("gmtModified", new Date(), metaObject);
+    }
+
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        this.setFieldValByName("gmtModified", new Date(), metaObject);
+    }
+}
