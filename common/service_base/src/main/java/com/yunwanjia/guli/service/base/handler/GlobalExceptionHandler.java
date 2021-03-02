4d1ba19cacc35e3929be4f210ca607ca8acb2ce5
lijw <113714806@qq.com>
2021-03-02 12:32:28 +0800

    add:新增统一异常处理

---
+++ common/service_base/src/main/java/com/yunwanjia/guli/service/base/handler/GlobalExceptionHandler.java
@@ -0,0 +1,40 @@
+package com.yunwanjia.guli.service.base.handler;
+
+import com.yunwanjia.guli.common.base.result.ResultCodeEnum;
+import com.yunwanjia.guli.common.base.result.ResultDTO;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.jdbc.BadSqlGrammarException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @author lijw
+ * 统一异常处理器
+ */
+@ControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    @ResponseBody
+    public ResultDTO error(Exception e) {
+        log.error(e.getMessage());
+        return ResultDTO.error().message(e.getMessage());
+    }
+
+    @ExceptionHandler(BadSqlGrammarException.class)
+    @ResponseBody
+    public ResultDTO error(BadSqlGrammarException e) {
+        log.error(e.getMessage());
+        return ResultDTO.setResult(ResultCodeEnum.BAD_SQL_GRAMMAR);
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    @ResponseBody
+    public ResultDTO error(HttpMessageNotReadableException e) {
+        log.error(e.getMessage());
+        return ResultDTO.setResult(ResultCodeEnum.JSON_PARSE_ERROR);
+    }
+}
